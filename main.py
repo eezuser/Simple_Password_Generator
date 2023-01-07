680811f8e3fc579a7a6a3f5aa92690f85e012f9a
Asad Khan <asad.k.khan@oracle.com>
2023-01-07 17:32:56 -0500

    deconstructed password generator functionality, leverged string lib to further randomize pw using special chars instead of just regular chars and ints, removed literal constraint of ints between 1-500000, removed error handling functionality (which can probably be improved upon in the future

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 # Password Generator
 import random
+import string 
 
+print('@@@@@@@@@@ Welcome to Wales password generator @@@@@@@@@@')
+
+# request user data
+length = int(input('\nEnter the length of password, we recommend 8 chars or more: '))
 first_name = input(str("First Name:"))
 last_name = input(str("Last Name:"))
-num = random.randint(1,500000)
 
-def password_generator(first_name, last_name, number):
-  t_password = first_name + last_name
-  e_password = t_password.encode(encoding="UTF-8",errors="backslashreplace")
-  return e_password
+# include random chars in the mixing of the pw 
+symbols = string.punctuation
 
-final_password = password_generator(first_name, str(num), last_name )
-print(" Your password is: " + str(final_password))
+# combine the data
+t_password = first_name + last_name + symbols
+
+# randomly form the pw
+temp = random.sample(t_password,length)
+
+# create the pw
+password_created = "".join(temp)
+
+# print the pw
+print(" Your password is: " + password_created)
 
